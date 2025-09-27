6dfbe50a9b9b1e3f279a87dc2fa768878e7bf84a
lien7806 <domailien7806@gmail.com>
2025-09-27 23:51:39 +0700

    Introduction

---
+++ HW1/src/Introduction.java
@@ -0,0 +1,10 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Đỗ Mai Liên\t24022677\tINT2204_6\tlien7806\t24022677@vnu.edu.vn");
+        for(int i=9;i>0;i--){
+            System.out.println(i+" bottles of beer on the wall, "+i+" bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
